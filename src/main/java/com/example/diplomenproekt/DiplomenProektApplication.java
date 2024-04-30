b1dd77dc55ec0354548f5db4a481af66c19fd59f
Kireto61 <kiril.valkov@abv.bg>
2024-05-01 00:23:17 +0300

    version 1

--- src/main/java/com/example/diplomenproekt/DiplomenProektApplication.java
+++ src/main/java/com/example/diplomenproekt/DiplomenProektApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DiplomenProektApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DiplomenProektApplication.class, args);
     }
